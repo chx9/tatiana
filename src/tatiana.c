b4b78408de8e375e7ea96f83d0216bcb8ebde6c6
chx9 <cheng.huan@icloud.com>
2024-07-06 21:07:47 +0800

    update

--- src/tatiana.c
+++ src/tatiana.c
@@ -1,6 +1,34 @@
-#include "sds.h"
 #include "ae.h"
-int main()
-{
-  return 0; 
+#include "anet.h"
+#include "sds.h"
+#include "hiredis/hiredis.h"
+#include <arpa/inet.h>
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+static int installIOHandler(aeEventLoop *eventLoop, int fd, int mask, aeFileProc *proc, void *clientData, int retried){
+  if(aeCreateFileEvent(eventLoop,  fd,  mask,  proc, clientData) != AE_ERR){
+    return 1;
+  }else{
+    if(!retried){
+      return installIOHandler(eventLoop,  fd,  mask,  *proc,  clientData,  retried);
+    }
+  }
+}
+void writeToRedis(struct aeEventLoop *eventLoop, int fd, void *clientData, int mask){
+  
+}
+int main() {
+  char err_msg[1024];
+  const char *addr = "127.0.0.1";
+  int port = 6379;
+  redisContext *ctx = redisConnectNonBlock(addr, port);
+  aeEventLoop* loop = aeCreateEventLoop(1024);
+  installIOHandler(loop,  ctx->fd,  AE_WRITABLE,  writeToRedis, NULL, 0);
+  return 0;
 }
